59c395fa1132f29a150e82f8ee45f8e61b6530fc
Giwoo Lee <hazel@Giwoo-Lee.local>
2021-11-17 23:11:07 +0900

    docs: Include usage for processfasta.py

--- python-genomic/week4/processfasta.py
+++ python-genomic/week4/processfasta.py
@@ -2,11 +2,25 @@
 """
 processfasta.py builds a dictionary with all sequences from a FASTA file.
 """
-
 import sys
-filename=sys.argv[1]
 
-try:
-    f = open(filename)
-except IOError:
-    print("File %s does not exist!!" % filename)
+def usage():
+    print("""
+    processfasta.py: reads a FASTA file and build a dictionary with all sequences
+    bigger than a given length.
+
+    processfasta.py [-h] [-l <length>] <filename>
+      -h             print this message
+      -l <length>    filter all sequences with a length
+                  smaller than <length>
+                  (default <length>=0)  
+      <filename>     the file has to be in FASTA format
+    """)
+
+if __name__ == "__main__":    
+    filename=sys.argv[1]
+
+    try:
+        f = open(filename)
+    except IOError:
+        print("File %s does not exist!!" % filename)
